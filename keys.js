cd4b4ef04ef7a075bb20ceea2e130181b64603d1
Bhagyarekha <bhagyarekha.deshpande@gmail.com>
2021-10-13 20:22:20 +0530

    key file changed

--- keys.js
+++ keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  MONGOURI:'mongodb://localhost:27017/MovieApp',
+  //MONGOURI:'mongodb://localhost:27017/MovieApp',
+  MONGOURI:"mongodb+srv://bhagyadeshpande:KRgzCp1x2UWXrQwy@cluster0.7iook.mongodb.net/MovieApp?retryWrites=true&w=majority",
   JWT_SECRET:"Ilovecoding"
 }
